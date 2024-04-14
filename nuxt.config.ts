6544d2cebdcbf9610333eb19866bcf1ab20d590a
PatrickHuizinga88 <patrickhuizinga44@gmail.com>
2024-04-14 21:59:35 +0200

    Added favicon variants

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,9 +9,16 @@ export default defineNuxtConfig({
         { name: 'og:title', content: 'Lapzy - Jouw MX performance platform' },
         { name: 'description', content: 'Check, deel en verbeter je rondetijden op elke crossbaan.' },
         { name: 'og:description', content: 'Check, deel en verbeter je rondetijden op elke crossbaan.' },
+        {name: 'msapplication-TileColor', content: '#ffffff'},
+        {name: 'theme-color', content: '#ffffff'},
       ],
       link: [
-        {rel: 'icon', type: 'image/x-icon', href: '/favicon.svg'}
+        {rel: 'icon', type: 'image/x-icon', href: '/favicon/favicon.svg'},
+        {rel: 'apple-touch-icon', sizes: '180x180', href: '/favicon/apple-touch-icon.png'},
+        {rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon/favicon-32x32.png'},
+        {rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon/favicon-16x16.png'},
+        {rel: 'manifest', href: '/favicon/site.webmanifest'},
+        {rel: 'mask-icon', href: '/favicon/safari-pinned-tab.svg', color: '#1e5fde'},
       ],
     },
   },
